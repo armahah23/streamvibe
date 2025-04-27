7a986a9bd64c123f7d6359d4846de698870d0484
A R M Afrih <151869356+armahah23@users.noreply.github.com>
2025-04-27 08:24:14 +0530

    feat: add Explore component and integrate it into Home page; update Hero component with logo and styling

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import hero_logo from "../assets/Abstract Design.png";
+import { GiPlayButton } from "react-icons/gi"
 
 function Hero() {
   return (
-    <div>
-      Hero
+    <div className="text-white bg-neutral-900 ">
+      <div className="relative w-full h-[400px] ">
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-neutral-900/50 to-neutral-900/90"></div>
+        <div className="absolute top-[-20px] left-0 w-full h-full flex items-center justify-center">
+          <img src={hero_logo} alt="logo" className="has-[200px] w-[300px]" />
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center ">
+        <h1 className="text-[48px]">The Best Streaming Experience</h1>
+        <p className="text-center text-[14px] max-w-[600px] mt-1 text-gray-400">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi quo
+          nulla, minus saepe quae autem ut architecto fuga minima, cupiditate a
+          fugiat rem odio quos, temporibus porro culpa ipsa iusto?
+        </p>
+        <div className="flex items-center justify-center gap-2 py-1 px-3 bg-red-600 mt-2 rounded-md text-lg">
+          <GiPlayButton />
+          <span className="text-[14px] font-bold">Start Watching Now</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
+import Explore from '../components/Explore'
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Explore />
     </>
   )
 }
